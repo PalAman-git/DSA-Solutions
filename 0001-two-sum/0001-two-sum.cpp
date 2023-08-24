20454071a0f500d46d00db3e5182b3b539e77c31
Aman Pal <96633574+PalAman-git@users.noreply.github.com>
2023-08-24 22:15:35 +0530

    Time: 7 ms (93.68%), Space: 10.9 MB (24.36%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        //it can be done with the time complexity of nlogn by first sorting the array
+        //and then using 2 pointers i and j if target is less then arr[i] + arr[j] greater,
+        //decrease the j and if the sum is less then increase i and find the pair
+
+        //this approach do this in n time and n space complexity
+
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(mp.find(target - nums[i]) == mp.end())
+            {
+                mp[nums[i]] = i;
+            }
+            else
+            {
+                return {mp[target - nums[i]],i};
+            }
+        }
+
+        return {-1,-1};
+    }
+};
