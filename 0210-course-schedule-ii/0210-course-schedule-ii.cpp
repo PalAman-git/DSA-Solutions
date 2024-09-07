295e4f277993e237d6d576a02bb29b5eb1b5deb3
Aman Pal <96633574+PalAman-git@users.noreply.github.com>
2024-09-07 19:26:29 +0530

    Time: 18 ms (39.4%), Space: 17.6 MB (64.51%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<int> findOrder(int N, vector<vector<int>>& prerequisites) {
+        //make the adjacency list for the courses 
+        vector<int> adj[N];
+        for(auto i : prerequisites)
+        {
+            adj[i[1]].push_back(i[0]);
+        }
+
+        //make the indegree for all the courses
+        vector<int> indegree(N,0);
+        for(int i=0;i<N;i++)
+        {
+            for(auto it: adj[i])
+            {
+                indegree[it]++;
+            }
+        }
+
+        //push all the nodes having indegree as 0
+        queue<int> q;
+        for(int i=0;i<N;i++)
+        {
+            if(indegree[i] == 0)
+            {
+                q.push(i);
+            }
+        }
+
+        //process the graph
+        vector<int> ans;
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for(auto i : adj[node])
+            {
+                indegree[i]--;
+                if(indegree[i] == 0)
+                {
+                    q.push(i);
+                }
+            }
+        }
+
+        if(ans.size() == N) return ans;
+
+        return {};
+    }
+};
