dd523e1a93048b727d4899a0ffc482be1c3a41cc
Aman Pal <96633574+PalAman-git@users.noreply.github.com>
2024-09-16 17:19:46 +0530

    Time: 0 ms (100%), Space: 15.5 MB (51.84%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -29,25 +29,59 @@ public:
     }
 }
 
-void solve(TreeNode* root, int k, vector<int>& ans, unordered_map<TreeNode*, TreeNode*>& parent, unordered_map<TreeNode*, bool>& visited) {
-    if (root == NULL || visited[root]) return;
+//Using DFS
+// void solve(TreeNode* root, int k, vector<int>& ans, unordered_map<TreeNode*, TreeNode*>& parent, unordered_map<TreeNode*, bool>& visited) {
+//     if (root == NULL || visited[root]) return;
 
-    // Mark this node as visited
-    visited[root] = true;
+//     // Mark this node as visited
+//     visited[root] = true;
 
-    // If distance k is reached, add the node to the answer
-    if (k == 0) {
-        ans.push_back(root->val);
-        return;
+//     // If distance k is reached, add the node to the answer
+//     if (k == 0) {
+//         ans.push_back(root->val);
+//         return;
+//     }
+
+//     // Recur for the left, right, and parent nodes
+//     solve(root->left, k - 1, ans, parent, visited);
+//     solve(root->right, k - 1, ans, parent, visited);
+    
+//     // Move to the parent node
+//     if (parent[root] != NULL) {
+//         solve(parent[root], k - 1, ans, parent, visited);
+//     }
+// }
+
+//Using BFS
+void solve(TreeNode* root,int k,vector<int> &ans,unordered_map<TreeNode*,TreeNode*> &parent,unordered_map<TreeNode*,bool> &visited)
+{
+    queue<TreeNode*> q;
+    q.push(root);
+    int dis = 0;
+
+    while(true)
+    {
+        int size = q.size();
+        if(dis == k) break;
+        
+        for(int i=0;i<size;i++)
+        {
+            TreeNode* node = q.front();
+            visited[node] = true;
+            q.pop();
+
+            if(node -> left && !visited[node -> left]) q.push(node -> left);
+            if(node -> right && !visited[node -> right]) q.push(node -> right);
+            if(parent[node] && !visited[parent[node]]) q.push(parent[node]);
+            
+        }
+        dis++;
     }
 
-    // Recur for the left, right, and parent nodes
-    solve(root->left, k - 1, ans, parent, visited);
-    solve(root->right, k - 1, ans, parent, visited);
-    
-    // Move to the parent node
-    if (parent[root] != NULL) {
-        solve(parent[root], k - 1, ans, parent, visited);
+    while(!q.empty())
+    {
+        ans.push_back(q.front() -> val);
+        q.pop();
     }
 }
 
