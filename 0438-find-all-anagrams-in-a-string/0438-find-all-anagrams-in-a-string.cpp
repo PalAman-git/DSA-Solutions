43b7e775a59517a5bdaa3521ff73068573ee29c9
Aman Pal <96633574+PalAman-git@users.noreply.github.com>
2024-09-29 21:32:38 +0530

    Time: 15 ms (43.56%), Space: 11.3 MB (45.69%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,36 +1,40 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        unordered_map<char,pair<int,int>> mp;
+        unordered_map<char,int> mp;
         int n = s.length();
         int k = p.length();
 
         for(int i=0;i<p.size();i++)
         {
-            mp[p[i]].first++;
-            mp[p[i]].second = 0;
+            mp[p[i]]++;
         }
+        int count = mp.size();//when this count will be zero that will mean that all the character in same quantity have been found so it is an anagram
+
 
         int i=0,j=0;
         vector<int> result;
         
         while(j < n)
         {
-            mp[s[j]].second++;
+            if(mp.find(s[j]) != mp.end()){
+                mp[s[j]]--;
+                if(mp[s[j]] == 0) count--;
+            }
 
             if(j-i+1 < k) j++;
 
             else if(j-i+1 == k){
                 //check for the valid anagram
-                int matched=0;
-                for(auto it:mp)
+                if(count == 0) result.push_back(i);
+
+                //removing the calculation for 
+                if(mp.find(s[i]) != mp.end())
                 {
-                   if(it.second.first == it.second.second) matched++;
+                    mp[s[i]]++;
+                    if(mp[s[i]] == 1) count++;
                 }
 
-                if(matched == mp.size()) result.push_back(i);
-
-                mp[s[i]].second--;
                 i++;
                 j++;
             }
