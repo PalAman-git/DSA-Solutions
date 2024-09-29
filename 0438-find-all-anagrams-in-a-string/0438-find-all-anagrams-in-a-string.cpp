891b5136c5d079c50099e99978af9945232d1740
Aman Pal <96633574+PalAman-git@users.noreply.github.com>
2024-09-29 21:33:16 +0530

    Time: 14 ms (47.12%), Space: 11.2 MB (47.5%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,40 +1,41 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        unordered_map<char,int> mp;
+        unordered_map<char, int> mp;
         int n = s.length();
         int k = p.length();
 
-        for(int i=0;i<p.size();i++)
-        {
+        for (int i = 0; i < p.size(); i++) {
             mp[p[i]]++;
         }
-        int count = mp.size();//when this count will be zero that will mean that all the character in same quantity have been found so it is an anagram
+        int count = mp.size(); // when this count will be zero that will mean
+                               // that all the character in same quantity have
+                               // been found so it is an anagram
 
-
-        int i=0,j=0;
+        int i = 0, j = 0;
         vector<int> result;
-        
-        while(j < n)
-        {
-            if(mp.find(s[j]) != mp.end()){
+
+        while (j < n) {
+            if (mp.find(s[j]) != mp.end()) {
                 mp[s[j]]--;
-                if(mp[s[j]] == 0) count--;
+                if (mp[s[j]] == 0)
+                    count--;
             }
 
-            if(j-i+1 < k) j++;
+            if (j - i + 1 < k)
+                j++;
 
-            else if(j-i+1 == k){
-                //check for the valid anagram
-                if(count == 0) result.push_back(i);
+            else if (j - i + 1 == k) {
+                // check for the valid anagram
+                if (count == 0)
+                    result.push_back(i);
 
-                //removing the calculation for 
-                if(mp.find(s[i]) != mp.end())
-                {
+                // removing the calculation for ith element
+                if (mp.find(s[i]) != mp.end()) {
                     mp[s[i]]++;
-                    if(mp[s[i]] == 1) count++;
+                    if (mp[s[i]] == 1)
+                        count++;
                 }
-
                 i++;
                 j++;
             }
