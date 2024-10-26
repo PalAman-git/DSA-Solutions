cd254050fe4cbbaa121d5513500c7bf8442121ed
Aman Pal <96633574+PalAman-git@users.noreply.github.com>
2024-10-26 18:59:28 +0530

    Time: 3 ms (96.95%), Space: 19.3 MB (5.74%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,24 +1,52 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> mp;
-        for(int i=0;i<nums.size();i++)
-        {
-            mp[nums[i]]++;
-        }
-        priority_queue<pair<int,int>> pq;
+        //using the max heap(NlogN)
+        // unordered_map<int,int> mp;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     mp[nums[i]]++;
+        // }
+        // priority_queue<pair<int,int>> pq;
 
+        // for(const auto &it : mp)
+        // {
+        //     pq.push({it.second,it.first});
+        // }
+        
+        // vector<int> ans;
+        // for(int i=0;i<k;i++)
+        // {
+        //     ans.push_back(pq.top().second);
+        //     pq.pop();
+        // }
+        // return ans;
+
+        //using the bucket sort
+        unordered_map<int,int> mp;
+        for(auto &it : nums)
+        {
+            mp[it]++;
+        }
+
+        vector<vector<int>> bucket(nums.size()+1);
         for(const auto &it : mp)
         {
-            pq.push({it.second,it.first});
+            int i = it.second;
+            int value = it.first;
+
+            bucket[i].push_back(value);
         }
-        
+
         vector<int> ans;
-        for(int i=0;i<k;i++)
+        for(int i=bucket.size()-1;i>=0;i--)
         {
-            ans.push_back(pq.top().second);
-            pq.pop();
+           for(int &num : bucket[i])
+           {
+            ans.push_back(num);
+            if(ans.size() == k) return ans;
+           }
         }
-        return ans;
+        return {0};
     }
 };
