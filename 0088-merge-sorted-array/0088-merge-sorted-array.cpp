dfc140ecd7ea121ea1b2bbfadff6d1294cf5e76a
Aman Pal <96633574+PalAman-git@users.noreply.github.com>
2025-08-29 12:36:06 +0530

    Time: 0 ms (100%), Space: 12.2 MB (70.35%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,36 +1,57 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> result(m+n);
+        // using the extra space;
+        //     vector<int> result(m+n);
 
-        int i = 0,j = 0;
-        int k = 0;
-        while(i < m && j < n){
-            if(nums1[i] < nums2[j]){
-                result[k] = nums1[i];
-                i++;
-            }else{
-                result[k] = nums2[j];
-                j++;
+        //     int i = 0,j = 0;
+        //     int k = 0;
+        //     while(i < m && j < n){
+        //         if(nums1[i] < nums2[j]){
+        //             result[k] = nums1[i];
+        //             i++;
+        //         }else{
+        //             result[k] = nums2[j];
+        //             j++;
+        //         }
+        //         k++;
+        //     }
+
+        //     while(i < m){
+        //         result[k] = nums1[i];
+        //         i++;
+        //         k++;
+        //     }
+
+        //     while(j < n){
+        //         result[k] = nums2[j];
+        //         j++;
+        //         k++;
+        //     }
+
+        //     for(int i=0;i<m+n;i++){
+        //         nums1[i] = result[i];
+        //     }
+
+        //    --------------------------------------------------------------------------------------------------
+        //    using two pointers to fill the nums1 array from the last position
+        //    , we will compare the last values of both nums1 and nums2 and put
+        //    the value that is greater and decrease the pointer of that array
+
+        int l1 = m - 1;
+        int l2 = n - 1;
+        int lastIdx = m + n - 1;
+
+        while (l2 >= 0) {
+            if(l1 >= 0 && nums1[l1] > nums2[l2]){
+                nums1[lastIdx] = nums1[l1];
+                l1--;
             }
-            k++;
-        }
-
-        while(i < m){
-            result[k] = nums1[i];
-            i++;
-            k++;
-        }
-
-        while(j < n){
-            result[k] = nums2[j];
-            j++;
-            k++;
-        }
-
-
-        for(int i=0;i<m+n;i++){
-            nums1[i] = result[i];
+            else{
+                nums1[lastIdx] = nums2[l2];
+                l2--;
+            }
+            lastIdx--;
         }
     }
 };
