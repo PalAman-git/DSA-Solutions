39dfbf0d1ca4ace756483ce63b9b2e7d0af415bc
Aman Pal <96633574+PalAman-git@users.noreply.github.com>
2025-09-30 11:42:56 +0530

    Time: 48 ms (89.32%), Space: 22.4 MB (74.81%) - LeetHub

--- 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.cpp
+++ 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.cpp
@@ -4,9 +4,9 @@ public:
         
         int n = nums.size();
 
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<n-1;i++){//no. of time the process will continue n-1 times
 
-            for(int j=0;j<n-1-i;j++){
+            for(int j=0;j<n-1-i;j++){// in each iteration just adding nums[j] , nums[j+1] and mod with 10 to get the last digit and storing at nums[j]
 
                 nums[j] = (nums[j] + nums[j+1]) % 10;
             }
