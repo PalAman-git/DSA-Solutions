d1fb8f225275f9eea169ea78633ed5107db8f60f
Aman Pal <96633574+PalAman-git@users.noreply.github.com>
2025-10-01 15:39:23 +0530

    Time: 0 ms (100%), Space: 7.9 MB (72.48%) - LeetHub

---
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string digits = to_string(num);
+
+        for(int i=0;i<digits.length();i++){
+            if(digits[i] == '6'){
+                digits[i] = '9';
+                break;
+            }
+        }
+
+        return stoi(digits);
+    }
+};
