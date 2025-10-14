a89e4e8bf4ac76a430679ea167d187a98e925bd8
Aman Pal <96633574+PalAman-git@users.noreply.github.com>
2025-10-14 16:19:37 +0530

    Time: 16 ms (41.59%), Space: 41.3 MB (19.47%) - LeetHub

--- 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.cpp
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
-    bool isStrictlyIncreasing(vector<int>& nums,int startIdx,int k){
-        for(int i=0;i<k - 1;i++){
-            if(nums[startIdx + i] >= nums[startIdx + i+1] )return false;
-        }
-        return true;
-    }
+    // bool isStrictlyIncreasing(vector<int>& nums,int startIdx,int k){
+    //     for(int i=0;i<k - 1;i++){
+    //         if(nums[startIdx + i] >= nums[startIdx + i+1] )return false;
+    //     }
+    //     return true;
+    // }
 
+    // bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+    //     int n = nums.size();
+
+    //     for(int i=0;i <= n - (k*2) ; i++ ){
+    //         if(isStrictlyIncreasing(nums,i,k) && isStrictlyIncreasing(nums,i+k,k)) return true;
+    //     }
+    //     return false;
+    // }
+
+    //my first solution O(n*k) , runs for loop k times for every n
+    
     bool hasIncreasingSubarrays(vector<int>& nums, int k) {
         int n = nums.size();
+        if (n < 2 * k) return false;
 
-        for(int i=0;i <= n - (k*2) ; i++ ){
-            if(isStrictlyIncreasing(nums,i,k) && isStrictlyIncreasing(nums,i+k,k)) return true;
+        vector<int> inc(n, 1);
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1])
+                inc[i] = inc[i - 1] + 1;
+        }
+
+        for (int i = k - 1; i + k < n; i++) {
+            // Check if the k-length streak ends here and next subarray also has streak of k
+            if (inc[i] >= k && inc[i + k] >= k) return true;
         }
         return false;
     }
