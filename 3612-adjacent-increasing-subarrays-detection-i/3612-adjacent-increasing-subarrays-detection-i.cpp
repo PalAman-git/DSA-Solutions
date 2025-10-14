e7deabc6e332be68b9ea3e05e9efae3088d660ac
Aman Pal <96633574+PalAman-git@users.noreply.github.com>
2025-10-14 16:21:26 +0530

    Time: 13 ms (55.31%), Space: 41.4 MB (10.18%) - LeetHub

--- 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.cpp
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.cpp
@@ -18,19 +18,20 @@ public:
 
     //my first solution O(n*k) , runs for loop k times for every n
     
+
     bool hasIncreasingSubarrays(vector<int>& nums, int k) {
         int n = nums.size();
-        if (n < 2 * k) return false;
+        if(n < 2*k) return false;
 
-        vector<int> inc(n, 1);
-        for (int i = 1; i < n; i++) {
-            if (nums[i] > nums[i - 1])
-                inc[i] = inc[i - 1] + 1;
+        vector<int> inc(n,1);
+
+        for(int i = 1;i<n;i++){
+            if(nums[i] > nums[i-1]) inc[i] = inc[i-1] + 1;
         }
 
-        for (int i = k - 1; i + k < n; i++) {
+        for(int i=k-1;i + k < n;i++){
             // Check if the k-length streak ends here and next subarray also has streak of k
-            if (inc[i] >= k && inc[i + k] >= k) return true;
+            if(inc[i] >= k && inc[i + k] >= k)return true;
         }
         return false;
     }
