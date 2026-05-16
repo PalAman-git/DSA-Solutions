ea41ba5c8fe9f72b061eb10fd7c6211901f01044
Aman Pal <96633574+PalAman-git@users.noreply.github.com>
2026-05-16 16:44:39 +0530

    Time: 71 ms (16.13%), Space: 21.6 MB (14.64%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    unordered_map<int,int> dp;
-    int solve(int i, vector<int>& nums) {
-
-        if(dp.find(i) != dp.end()) return dp[i];
-
+    int jump(vector<int>& nums) {
         int n = nums.size();
+        vector<int> dp(n, 1e9);
 
-        if (i >= n - 1)
-            return 0;
+        // reaching index 0 takes 0 jumps as we are alredy standing there
+        dp[0] = 0;
 
-        int ans = 1e9;
-        for (int jump = 1; jump <= nums[i]; jump++) {
-            ans = min(ans, 1 + solve(i + jump, nums));
+        for (int i = 0; i < n; i++){
+
+            for(int jump = 1;jump<= nums[i];jump++){
+                int nextIndex = i + jump;
+
+                if(nextIndex < n){
+                    dp[nextIndex] = min(dp[nextIndex],1 + dp[i]);
+                }
+            }
         }
 
-        dp[i] = ans;
-        return ans;
+        return dp[n-1];
     }
-    int jump(vector<int>& nums) { return solve(0, nums); }
 };
